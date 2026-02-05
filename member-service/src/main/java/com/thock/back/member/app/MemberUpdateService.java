a86a0728b74337c17381b417fb09280286f44bb7
Eom Sanghyun <quarterMoon234@gmail.com>
2026-02-05 17:45:45 +0900

    chore: MemberUpdateService.java @Transactional import 변경

--- member-service/src/main/java/com/thock/back/member/app/MemberUpdateService.java
+++ member-service/src/main/java/com/thock/back/member/app/MemberUpdateService.java
@@ -8,9 +8,9 @@ import com.thock.back.member.domain.entity.Member;
 import com.thock.back.member.out.MemberRepository;
 import com.thock.back.shared.member.domain.MemberRole;
 import com.thock.back.shared.member.event.MemberModifiedEvent;
-import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
@@ -20,13 +20,13 @@ public class MemberUpdateService {
     private final MemberRepository memberRepository;
     private final EventPublisher eventPublisher;
 
-    public void updateMemberRole(Long memberId, String bankCode, String accountNumber, String accountHolder) throws Exception {
+    public void updateMemberRole(Long memberId, String bankCode, String accountNumber, String accountHolder) {
         Member member = memberRepository.findById(memberId)
                 .orElseThrow(() -> new CustomException(ErrorCode.MEMBER_NOT_FOUND));
 
         String memberRole = member.getRole().toString();
 
-        if (memberRole.equals("USER")) {
+        if (member.getRole() == MemberRole.USER) {
             member.setRole(MemberRole.SELLER);
             member.setBankCode(bankCode);
             member.setAccountNumber(accountNumber);
@@ -35,7 +35,7 @@ public class MemberUpdateService {
 
             eventPublisher.publish(new MemberModifiedEvent(member.toDto()));
         } else {
-            throw new Exception();
+            throw new CustomException(ErrorCode.INVALID_ROLE_PROMOTION);
         }
     }
 }
