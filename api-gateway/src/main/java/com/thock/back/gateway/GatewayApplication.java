75cf5a5c8a5e3f9c414e487fd44264b0741e293c
Eom Sanghyun <quarterMoon234@gmail.com>
2026-02-11 10:57:30 +0900

    feat: gateway에서 common 모듈 참조시 WebMVC 충돌 방지 세팅

--- api-gateway/src/main/java/com/thock/back/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/com/thock/back/gateway/GatewayApplication.java
@@ -3,14 +3,37 @@ package com.thock.back.gateway;
 import com.thock.back.global.security.JwtProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 
-import javax.swing.*;
-
-@SpringBootApplication(scanBasePackages = {
-        "com.thock.back.gateway",
-        "com.thock.back.global",
-})
+@SpringBootApplication(
+        scanBasePackages = {
+                "com.thock.back.gateway"
+        },
+        exclude = {
+                DataSourceAutoConfiguration.class,
+                HibernateJpaAutoConfiguration.class
+        }
+)
+@ComponentScan(
+        basePackages = {
+                "com.thock.back.gateway",
+                "com.thock.back.global.security"
+        },
+        excludeFilters = {
+                @ComponentScan.Filter(
+                        type = FilterType.REGEX,
+                        pattern = "com\\.thock\\.back\\.global\\.config\\..*"
+                ),
+                @ComponentScan.Filter(
+                        type = FilterType.REGEX,
+                        pattern = "com\\.thock\\.back\\.global\\.security\\.(SecurityConfig|JwtAuthenticationFilter)"
+                )
+        }
+)
 @EnableConfigurationProperties(JwtProperties.class)
 public class GatewayApplication {
 
