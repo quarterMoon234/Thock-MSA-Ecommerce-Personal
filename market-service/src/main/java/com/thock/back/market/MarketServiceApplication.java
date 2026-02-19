ebfbd4c2da208bc4b27c85616111ef5f6f1cbe34
thrkrif <103479629+thrkrif@users.noreply.github.com>
2026-02-20 00:23:13 +0900

    feat: member/market에 @EnableJpaRepositories, @EntityScan 추가

--- market-service/src/main/java/com/thock/back/market/MarketServiceApplication.java
+++ market-service/src/main/java/com/thock/back/market/MarketServiceApplication.java
@@ -2,7 +2,9 @@ package com.thock.back.market;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication(scanBasePackages = {
@@ -10,6 +12,14 @@ import org.springframework.scheduling.annotation.EnableScheduling;
         "com.thock.back.global"
 })
 @EnableJpaAuditing
+@EnableJpaRepositories(basePackages = {
+        "com.thock.back.market",
+        "com.thock.back.global.outbox.repository"
+})
+@EntityScan(basePackages = {
+        "com.thock.back.market",
+        "com.thock.back.global.outbox.entity"
+})
 @EnableScheduling
 public class MarketServiceApplication {
 
